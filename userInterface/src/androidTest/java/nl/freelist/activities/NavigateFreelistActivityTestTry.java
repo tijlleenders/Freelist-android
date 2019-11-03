156ecbd8affccd6c896f1537e3a440954faeb6e8
Tijl Leenders <44887755+tijlleenders@users.noreply.github.com>
2019-11-03 20:33:44 +0100

    Created working proof of concept UI test

--- userInterface/src/androidTest/java/nl/freelist/activities/NavigateFreelistActivityTestTry.java
+++ userInterface/src/androidTest/java/nl/freelist/activities/NavigateFreelistActivityTestTry.java
@@ -1,15 +1,24 @@
 package nl.freelist.activities;
 
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
-
+import nl.freelist.freelist.R;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -17,28 +26,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import nl.freelist.freelist.R;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class NavigateFreelistActivityTestCreatePurpose {
+public class NavigateFreelistActivityTestTry {
 
   @Rule
   public ActivityTestRule<NavigateFreelistActivity> mActivityTestRule = new ActivityTestRule<>(
       NavigateFreelistActivity.class);
 
   @Test
-  public void navigateFreelistActivityTestCreatePurpose() {
+  public void navigateFreelistActivityTestTry() {
     ViewInteraction floatingActionButton = onView(
         allOf(withId(R.id.button_add_entry),
             childAtPosition(
@@ -57,17 +54,23 @@ public class NavigateFreelistActivityTestCreatePurpose {
                     0),
                 1),
             isDisplayed()));
-    appCompatEditText.perform(replaceText("p"), closeSoftKeyboard());
+    appCompatEditText.perform(typeText("hard"));
 
-    ViewInteraction editText = onView(
-        allOf(withId(R.id.edit_text_title), withText("p"),
+    ViewInteraction appCompatImageButton = onView(
+        allOf(withContentDescription("Navigate up"),
             childAtPosition(
-                childAtPosition(
-                    withId(android.R.id.content),
-                    0),
+                allOf(withId(R.id.action_bar),
+                    childAtPosition(
+                        withId(R.id.action_bar_container),
+                        0)),
                 1),
             isDisplayed()));
-    editText.check(matches(withText("p")));
+    appCompatImageButton.perform(click());
+
+    onView(new RecyclerViewMatcher(R.id.recycler_view)
+        .atPositionOnView(0, R.id.text_view_title))
+        .check(matches(withText("hard")))
+        .perform(click());
   }
 
   private static Matcher<View> childAtPosition(
